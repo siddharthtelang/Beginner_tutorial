0f0524a6af9b95416536e455aa2d95492c6a02aa
Siddharth Telang <stelang@umd.edu>
2021-11-08 06:57:51 -0500

    Add client functionality

--- src/listener.cpp
+++ src/listener.cpp
@@ -14,6 +14,7 @@
 Listener::Listener(ros::NodeHandle *nh_) {
   nh = nh_;
   init();
+  initServiceClient();
 }
 
 Listener::~Listener() {
@@ -22,6 +23,10 @@ Listener::~Listener() {
 
 void Listener::listenerCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("ROS LISTENER HEARD: [%s]", msg->data.c_str());
+  srv.request.str1 = msg->data;
+  if (client.call(srv)) {
+    ROS_INFO_STREAM("something" << srv.response.str2);
+  }
 }
 
 void Listener::init() {
@@ -30,4 +35,9 @@ void Listener::init() {
 
 void Listener::startNode() {
   ros::spin();
+}
+
+void Listener::initServiceClient() {
+  client = nh->serviceClient<beginner__tutorials::modify_Message>
+          ("/modify_Message");
 }
